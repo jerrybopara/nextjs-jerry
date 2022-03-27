605ee07669cb7422fa1a4add61217a0286abab8e
Jery Bopara <jerrybopara@gmail.com>
2022-03-27 16:10:38 +0530

    Changes

--- deploy.sh
+++ deploy.sh
@@ -1,20 +1,19 @@
 echo "Kill all the running PM2 actions"
-## sudo pm2 kill
+sudo pm2 kill
 
 echo "Jump to app folder"
 cd /home/ubuntu/next-js/
-echo "HI" >> helo.txt
 
 echo "Update app from Git"
-## git pull
+git pull
 
 echo "Install app dependencies"
-## sudo rm -rf node_modules package-lock.json
-## sudo npm install
+sudo rm -rf node_modules package-lock.json
+sudo npm install
 
 echo "Build your app"
-## sudo npm run build
+sudo npm run build
 
 echo "Run new PM2 action"
-## sudo cp /home/ubuntu/ecosystem.json ecosystem.json
-## sudo pm2 start ecosystem.json
+sudo cp /home/ubuntu/ecosystem.json ecosystem.json
+sudo pm2 start ecosystem.json
